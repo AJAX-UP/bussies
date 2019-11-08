ccf40e75220489476760142cc22e1d7a812b058e
czl960612 <18513348915@163.com>
2019-11-08 18:47:36 +0800

    Initial commit

---
+++ src/main/java/com/xcgn/bussies/BussiesApplication.java
@@ -0,0 +1,13 @@
+package com.xcgn.bussies;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BussiesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BussiesApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/xcgn/bussies/BussiesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xcgn.bussies;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BussiesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
